fa549f5fe99fbc2cdc8afc61b87b5c7585f20f3d
Joel Lonbeck <joel@neutrinographics.com>
2016-08-09 12:16:57 -0700

    updated the deployment script

--- bin/deploy.js
+++ bin/deploy.js
@@ -7,10 +7,7 @@ var templateDir = path.normalize(path.join(__dirname, '../templates'));
 
 var atomar_path = lib.lookup_module('atomar');
 
-// TODO: make the atomar path optional and default to use the global location
-// TODO: make the app path optional and default to use the current directory (but check that it's valid first)
-
-exports.command = 'deploy <deploy_path> <atomar_path>';
+exports.command = 'deploy <deploy_path>';
 exports.describe = 'Deploys an atomar application to run on an atomar instance';
 exports.builder = {
     app: {
@@ -29,13 +26,22 @@ exports.handler = function(argv) {
     var app_path = path.resolve(argv.app_path);
     var atomar_path = path.resolve(argv.atomar_path);
 
-    // TODO: validate app_path is a valid atomar module
+    // validate app_path is a valid atomar module
+    try {
+        var app_package = require(path.join(app_path, 'package.json'));
+        if(!app_package.atomarVersion || !app_package.name) {
+            throw new Error();
+        }
+    } catch(err) {
+        throw new Error(app_path + ' is not a valid atomar module.');
+    }
 
+    // validate atomar installation
     if(!atomar_path) {
         throw new Error('An installation of atomar could not be found. Please install atomar globally or specify a custom atomar installation path with --atomar_path');
     }
 
-    console.log('Deploying to ' + deploy_path);
+    console.log('Deploying "' + app_package.name + '" to ' + deploy_path);
     shell.exec('mkdir -p ' + deploy_path);
     fs.writeFileSync(path.join(deploy_path, 'index.php'), '<?php\n\n' +
         'require_once("'+ path.resolve(atomar_path) + '/Atomar.php");\n\n' +
