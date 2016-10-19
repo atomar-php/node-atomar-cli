6fde28de814f5f9c8839be3999af861efcf49ad6
Joel Lonbeck <joel@neutrinographics.com>
2016-10-18 22:39:18 -0700

    fix

--- bin/add/controller/templates/api.php
+++ bin/add/controller/templates/api.php
@@ -7,11 +7,11 @@ use atomar\core\ApiController;
 class {{name}} extends ApiController {
 
     function get_hello_world() {
-        return 'getting: Hello, World! This is the {{module_id}} api.';
+        return 'getting: Hello, World! This is the {{name}} api.';
     }
 
     function post_hello_world() {
-        return 'posting: Hello, World! This is the {{module_id}} api.';
+        return 'posting: Hello, World! This is the {{name}} api.';
     }
 
     /**
