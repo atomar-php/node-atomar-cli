14e1a726ed340acb29b17a5b3065f6736dc3041d
Joel Lonbeck <joel@neutrinographics.com>
2016-11-08 17:11:58 -0800

    added support for installing dependencies

--- bin/install.js
+++ bin/install.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var lib = require('../lib');
 
-exports.command = 'install <name>';
-exports.describe = 'Installs an atomar module';
+exports.command = 'install [module]';
+exports.describe = 'Installs an atomar module or installs all the dependencies found an atomar.json file';
 exports.builder = {
     global: {
         alias: 'g',
@@ -15,5 +15,18 @@ exports.builder = {
     }
 };
 exports.handler = function(argv) {
-    lib.install_module(argv.name, argv.g ? null : 'atomar_modules', argv.ssh);
+    if(argv.module) {
+        lib.install_module(argv.module, argv.g ? null : 'atomar_modules', argv.ssh);
+    } else {
+        // install dependencies
+        if(lib.fileExists('atomar.json')) {
+            var data = fs.readFileSync('atomar.json', 'utf8');
+            var dependencies = JSON.parse(data).dependencies;
+            if(dependencies) {
+                for(var module in dependencies) {
+                    lib.install_module(module, 'atomar_modules', argv.ssh);
+                }
+            }
+        }
+    }
 };
