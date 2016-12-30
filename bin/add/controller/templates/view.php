551b8f0a5914f392fd3dffff3ac30e1a443a4013
Joel Lonbeck <joel@neutrinographics.com>
2016-12-29 16:35:29 -0800

    added support for paths in view classes

--- bin/add/controller/templates/view.php
+++ bin/add/controller/templates/view.php
@@ -7,7 +7,7 @@ use atomar\core\Controller;
 class {{name}} extends Controller {
 
     function GET($matches = array()) {
-        echo $this->renderView('{{module_id}}/views/{{html_view}}.html', array(
+        echo $this->renderView('@{{module_id}}/{{html_view}}.html', array(
             'greeting' => 'Hello, World! This is the {{name}} view.'
         ));
     }
--- bin/add/controller/view.js
+++ bin/add/controller/view.js
@@ -14,29 +14,50 @@ exports.builder = {
 exports.handler = function(argv) {
     let info = atomar_config.loadPackage();
     if(!info) throw new Error('Not an Atomar module. Try running inside a module.');
-    argv.name = argv.name.replace(/\.php$/i, '');
+    let name = argv.name.replace(/\.php$/i, '').replace(/^\/+/, '');
+    let relativePath = '';
 
-    let className = tools.className(argv.name);
-    let destFile = path.join(process.cwd(), atomar_config.controllers_dir, className + '.php');
-    mkdirp.sync(path.dirname(destFile));
-    if(tools.fileExists(destFile)) {
-        console.error('The path already exists', destFile);
+    // extract path
+    if(name.indexOf('/')) {
+        relativePath = path.dirname(name);
+        name = path.basename(name);
+    }
+
+    // prepare paths
+    let templates = path.join(__dirname, 'templates');
+    let className = tools.className(name);
+    let classDir = atomar_config.controllers_dir;
+    let viewDir = atomar_config.views_dir;
+    if(relativePath !== '') {
+        classDir = path.join(classDir, relativePath);
+        viewDir = path.join(viewDir, relativePath);
+    }
+    let classFile = path.join(process.cwd(), classDir,  className + '.php');
+    let viewFile = path.join(process.cwd(), viewDir, className.toLowerCase() + '.html');
+
+    // skip duplicates
+    if(tools.fileExists(classFile)) {
+        console.error('The path already exists', classFile);
         return;
     }
 
-    let templates = path.join(__dirname, 'templates');
-    tools.injectTemplate(path.join(templates, 'view.php'), destFile, {
-        namespace: info.name + '\\controller',
+    mkdirp.sync(path.dirname(viewFile));
+    mkdirp.sync(path.dirname(classFile));
+
+    // add controller
+    tools.injectTemplate(path.join(templates, 'view.php'), classFile, {
+        namespace: info.name + '\\' + classDir.replace(/\//, '\\'),
         name: className,
         module_id: info.name,
-        html_view: className.toLowerCase()
+        html_view: path.join(viewDir, className.toLowerCase())
     });
-    let viewFile = path.join(process.cwd(), atomar_config.views_dir, className.toLowerCase() + '.html');
-    mkdirp.sync(path.dirname(viewFile));
+
+    // add view
     if(!tools.fileExists(viewFile)) {
         tools.injectTemplate(path.join(templates, 'view.html'), viewFile);
     }
 
+    // add route
+    //  TODO: check if adding routes is disabled
 
-    // TODO: ask to build the route if one does not exist
 };
